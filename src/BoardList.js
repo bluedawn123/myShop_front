ec29f311aaeefb16f06aed0c7b02c3109737610e
bluedawn123 <haemilyjh@naver.com>
2025-01-07 20:19:16 +0900

    css 변경

--- src/BoardList.js
+++ src/BoardList.js
@@ -1,123 +1,103 @@
-import React, { Component } from 'react'
+import React, { Component } from 'react';
 import Table from 'react-bootstrap/Table';
 import Button from 'react-bootstrap/Button';
 import Axios from 'axios';
-import Form from 'react-bootstrap/Form'
-// const submitTest = () =>{
-//     Axios.get('http://localhost:8000/')
-//     .then(function(res){
-//         alert('등록완료');
-//         console.log(res);
-//     })
-//     .catch(function (error){
-//         console.log(error);
-//     })
-// }
+import Form from 'react-bootstrap/Form';
+// import "../styles/LoginPage.css";
 
-class Board extends Component{
-  render(){
-    return(
+// Board Component: 단일 행을 렌더링
+class Board extends Component {
+  render() {
+    const { id, title, registerId, date, onCheckboxChange } = this.props;
+
+    return (
       <tr>
         <td>
           <Form.Check 
             type="checkbox"
-            id ={`default-checkbox`}
-            value={this.props.id}  //추후 넘겨줄때 필요. 아래 e.target.value에서 사용
-            //여기서 체크가 되었는지 안 되었는지 알게해줘야한다. => onCheckboxChange함수사용
-            //자식이 부모와 소통하려면? 자식이 부모에게 데이터를 전달하려면, 부모가 전달한 함수를 자식이 호출하는 방식으로 해결
-            onChange={this.props.onCheckboxChange}
+            id={`default-checkbox-${id}`}
+            value={id}
+            onChange={onCheckboxChange}
           />
         </td>
-        <td>{this.props.id}</td>
-        <td>{this.props.title}</td>
-        <td>{this.props.registerId}</td>
-        <td>{this.props.date}</td>
+        <td>{id}</td>
+        <td>{title}</td>
+        <td>{registerId}</td>
+        <td>{date}</td>
       </tr>
-      )
-    }
+    );
+  }
 }
 
-
+// BoardList Component: 전체 테이블 렌더링
 export default class BoardList extends Component {
   state = {
-    BoardList : [],
-    checkList : []
-  }
+    BoardList: [],
+    checkList: []
+  };
 
+  // 체크박스 변경 이벤트 핸들러
   onCheckboxChange = (e) => {
-    const list = [...this.state.checkList];
-    list.push(e.target.value);
-    this.setState({
-      checkList : list  
-    })
+    const { value } = e.target;
+    this.setState((prevState) => ({
+      checkList: [...prevState.checkList, value]
+    }), () => {
+      console.log(this.state.checkList);
+    });
+  };
 
-    console.log(this.state.checkList)
-
-  }
-  
+  // 서버에서 게시판 목록 가져오기
   getList = () => {
-    Axios.get('http://localhost:8000/list')  //node의 8000/list는 const sql = "SELECT * FROM board"; 실행
-    .then((res) => {
-        //const data = res.data;
-        //console.log(data);  [{…}, {…}, {…}] 형식이므로,
-        //destructuring을 할수 있다.
-        const {data} = res;  //이거로 떄려박으려고 초깃값 BoardList를 바꾼다
-        console.log(data); 
+    Axios.get('http://localhost:8000/list')
+      .then((res) => {
+        const { data } = res;
+        console.log(data);
+        this.setState({ BoardList: data });
+      })
+      .catch((error) => {
+        console.error(error);
+      });
+  };
 
-        this.setState({
-            BoardList:data
-        })
-        
-    })
-    .catch((error) => {
-        console.log(error);
-    })
-  }
-
-
-
-  //ui와 axois의 결과가 mount된 이후 나와야하므로,
-  componentDidMount(){
+  // 컴포넌트가 마운트되었을 때 서버로부터 데이터 가져오기
+  componentDidMount() {
     this.getList();
   }
 
   render() {
-    //console.log(this.state.BoardList) // [{},{},{}] 로 들어가있다.
-    return (
-    <>
-    <Table striped bordered hover>
-        <thead>
-            <tr>
-            <th>번호</th>
-            <th>제목</th>
-            <th>작성자</th>
-            <th>작성일</th>
-            </tr>
-        </thead>
-        <tbody>
-            {
-                this.state.BoardList.map( (item, idx) => {
-                    return (
-                        <Board 
-                        key={item.BOARD_ID} 
-                        id={item.BOARD_ID}
-                        title={item.BOARD_TITLE}
-                        registerId={item.REGISTER_ID} 
-                        date={item.REGISTER_DATE}
-                        onCheckboxChange={this.onCheckboxChange}
-                        ></Board>
-                    )
-                })
-            }            
+    const { BoardList } = this.state;
 
-        </tbody>
-    </Table>
-    <div className='d-flex gap-1'>
-        <Button variant='primary' >글쓰기</Button>
-        <Button variant='secondary'>수정하기</Button>
-        <Button variant='danger'>삭제하기</Button>
-    </div>
-    </>
-    )
+    return (
+      <>
+        <Table striped bordered hover>
+          <thead>
+            <tr>
+              <th>체크박스</th>
+              <th>번호</th>
+              <th>제목</th>
+              <th>작성자</th>
+              <th>작성일</th>
+            </tr>
+          </thead>
+          <tbody>
+            {BoardList.map((item) => (
+              <Board 
+                key={item.BOARD_ID}
+                id={item.BOARD_ID}
+                title={item.BOARD_TITLE}
+                registerId={item.REGISTER_ID}
+                date={item.REGISTER_DATE}
+                onCheckboxChange={this.onCheckboxChange}
+              />
+            ))}
+          </tbody>
+        </Table>
+        <div className="d-flex gap-1 mt-3">
+          <Button variant="primary">글쓰기</Button>
+          <Button variant="secondary">수정하기</Button>
+          <Button variant="danger">삭제하기</Button>
+        </div>
+      </>
+    );
   }
 }
